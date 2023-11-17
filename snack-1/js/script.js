aff7762051ddba07df9083ba1b0c71d95f34772d
UmbertoSerino <serumberto@gmail.com>
2023-11-17 11:51:04 +0100

    snack complete

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,10 +1,16 @@
+// creiamo un array vuoto
 const arrayVuoto = []
-
+// partiamo da 0
 let number = 0;
 
-while (number < 50){
-    let userNumber = parseInt(prompt("Dammi i tuoi numeri!!"));
-    arrayVuoto.push(userNumber);
-    console.log(arrayVuoto);
 
+while (number <= 50){
+    let userNumber = parseInt(prompt("Dammi i tuoi numeri!!"));
+
+    if (!isNaN (userNumber)){
+        number += userNumber;
+        arrayVuoto.push(userNumber);
+    }
 }
+
+console.log(arrayVuoto, number);
