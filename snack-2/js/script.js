4abb78c3ef7dc880dd7ec628979eb485a8e5f47c
UmbertoSerino <serumberto@gmail.com>
2023-11-17 12:06:47 +0100

    created array

---
+++ snack-2/js/script.js
@@ -0,0 +1,9 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
+
+// generiamo un array con dei nomi
+const name = ["Alfa", "Beta", "Gamma"];
+// generiamo un altro array con dei cognomi
+const surname = ["Delta", "Omega", "Epsilon"];
+// generiamo un array vuoto dove inseriremo po una lista random 
+const secretList = [];
+// potremmo usare un ciclo while
