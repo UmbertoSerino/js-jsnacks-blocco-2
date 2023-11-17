bc462d78a4b657bb9a8895f181b8023687c52e5a
UmbertoSerino <serumberto@gmail.com>
2023-11-17 11:28:59 +0100

    created cycle while

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,6 +1,10 @@
 const arrayVuoto = []
 
 let number = 0;
-let userNumber = parseInt(prompt("Dammi i tuoi numeri!!"));
 
-while ()
+while (number < 50){
+    let userNumber = parseInt(prompt("Dammi i tuoi numeri!!"));
+    arrayVuoto.push(userNumber);
+    console.log(arrayVuoto);
+
+}
