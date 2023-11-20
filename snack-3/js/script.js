1736f1701decd9b3358864c12780eb6cdd40a02b
UmbertoSerino <serumberto@gmail.com>
2023-11-20 11:50:04 +0100

    function complete

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -3,9 +3,11 @@
 function word(firstWord, secondWord) {
     if (firstWord.length == secondWord.length){
         console.log(firstWord, secondWord);
-    } else {}
-        
-    
+    } else if (firstWord.length > secondWord.length) {
+        console.log(firstWord)
+    } else {
+        console.log(secondWord);
+    }   
 };
 
 
