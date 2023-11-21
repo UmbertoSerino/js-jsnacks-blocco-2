5e8d4dd4e7b017b96f23e060de10de979541ebff
UmbertoSerino <serumberto@gmail.com>
2023-11-21 12:14:00 +0100

    created function square

---
+++ snack-6/js/script.js
@@ -0,0 +1,15 @@
+// Chiedo all'utente un numero e genero attraverso una funzione un numero di quadrati equivalente al numero inserito dall'utente nel DOM, in qualsiasi forma. 
+
+const userNumber = parseInt(prompt("Dammi un numero!"));
+console.log(userNumber);
+
+
+function createdSquare(number) {
+    for (let index = 0; index < number; index++) {
+        const element = array[index];
+        const squareItem = document.createdElement("div");
+        
+    }
+    
+}
+
