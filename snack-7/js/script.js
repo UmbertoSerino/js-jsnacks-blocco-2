8e023e883f7f1a89327ef520fb73fc6aa649ecc7
UmbertoSerino <serumberto@gmail.com>
2023-11-24 16:23:25 +0100

    Morning Snack

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -2,65 +2,52 @@
 
 const zucchineList = [
 
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
-    }
-    tipo = {
-        colore: "verde",
-        peso: "80kg",
-        lunghezza: "5cm"
-        prezzo: "5€"
+    {   variety : "black", 
+        weight : "1kg",
+        length: "1m"
+    },
+    {   variety : "purple", 
+        weight : "3kg", 
+        length: "2m"
+    },
+    {   variety : "pink", 
+        weight : "4kg", 
+        length: "1m"
+    },
+    {   variety : "black", 
+        weight : "5kg", 
+        length: "1m"
+    },
+    {   variety : "dark", 
+        weight : "4kg", 
+        length: "1m"
+    },
+    {   variety : "gray", 
+        weight : "2kg", 
+        length: "1m"
+    },
+    {   variety : "magenta", 
+        weight : "1.5kg", 
+        length: "1m"
+    },
+    {   variety : "lime", 
+        weight : "2kg", 
+        length: "1m"
+    },
+    {   variety : "black", 
+        weight : "3kg", 
+        length: "1m"
+    },
+    {   variety : "black", 
+        weight : "4kg", 
+        length: "1m"
     }
 
-]
+];
+
+let sumWeight = 0;
+
+for (let index = 0; index < zucchineList.length; index++) {
+    sumWeight += parseFloat(zucchineList[index].weight);
+}
+console.log(sumWeight);
